3071181968e60e47d148f94e6bee709ffdb8f370
PatricSQuad <52120154+PatricSQuad@users.noreply.github.com>
2019-06-24 13:01:16 +0700

    Add files via upload

---
+++ asci.py
@@ -0,0 +1,17 @@
+import os
+os.system('clear')
+nama = raw_input("Nama: ")
+logo ='\033[36m╔═╗╔═╗╔╗╔╦  ╦╔═╗╦═╗╔╦╗ asci💙\033[37m v\033[36m0.1\n║  ║ ║║║║╚╗╔╝║╣ ╠╦╝ ║   \033[37m\n╚═╝╚═╝╝╚╝ ╚╝ ╚═╝╩╚═ ╩\n[\033[32m•\033[37m\033[37m]\033[37m Autor    \033[36m: \033[37mgOdenBngsd\n    WhatsApp \033[36m: \033[37m+6281586610616\n    Facebook \033[36m: \033[37mhttps://m.facebook.com/05Asiyap\n    Github   \033[36m: \033[37mhttps://github.com/PatricSQuad'
+city = u'{}'.format(nama) #<-/// masukan kode assci disini ///#
+utf8_encoded = city.encode('utf-8')
+print(logo)
+print()
+print('\033[37m[\033[32m√\033[37m] Kode  \033[36m:  \033[36m')
+print(repr(utf8_encoded))
+decoded_city = utf8_encoded.decode('utf-8')
+print()
+print('\033[37m[\033[32m√\033[37m] Hasil \033[36m:  \033[37m ')
+print(decoded_city)
+print()
+print()
+print('\033[36mGoodBye Friends\033[37m...')
